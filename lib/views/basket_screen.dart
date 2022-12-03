ee9976bdfb87f996c000ca01e81570d6aa35a54e
Umutcan <73028201+softwareTR-Can@users.noreply.github.com>
2022-12-03 19:34:16 +0300

    add Basket Screen

---
+++ lib/views/basket_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sezinsoft_demo/constants.dart';
+import 'package:sezinsoft_demo/widget/basket_widget.dart';
+import 'package:sezinsoft_demo/widget/product_card.dart';
+import 'package:sezinsoft_demo/widget/username_widget.dart';
+
+class BasketScreen extends StatefulWidget {
+  const BasketScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BasketScreen> createState() => _BasketScreenState();
+}
+
+class _BasketScreenState extends State<BasketScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              buildBasketWidget(),
+              buildUsernameWidget(),
+              buildBasketDescription(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  buildBasketDescription() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 15),
+        Text('Sepetim', style: kTextStyle.copyWith(fontSize: 24)),
+        Text('Tutar: ${tutar.toStringAsFixed(2).replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => "${m[1]},")} TL',
+            style: kTextStyle.copyWith(fontSize: 24)),
+      ],
+    );
+  }
+}
--- lib/widget/basket_widget.dart
+++ lib/widget/basket_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sezinsoft_demo/controller/basket_controller.dart';
+import 'package:sezinsoft_demo/views/basket_screen.dart';
 
 import '../constants.dart';
 import 'product_card.dart';
@@ -8,29 +9,34 @@ import 'product_card.dart';
 buildBasketWidget() {
   BasketController controller = BasketController();
 
-  return Row(
-    children: [
-      const Spacer(),
-      Container(
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(50),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              const Icon(Icons.shopping_basket_rounded, color: kIconColor),
-              const SizedBox(width: 20),
-              Obx(
-                () => Text(
-                    "${tutar.toStringAsFixed(2).replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => "${m[1]},")} TL",
-                    style: kTextStyle.copyWith(color: Colors.white)),
-              )
-            ],
+  return GestureDetector(
+    onTap: () {
+      Get.to(BasketScreen());
+    },
+    child: Row(
+      children: [
+        const Spacer(),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(50),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                const Icon(Icons.shopping_basket_rounded, color: kIconColor),
+                const SizedBox(width: 20),
+                Obx(
+                  () => Text(
+                      "${tutar.toStringAsFixed(2).replaceAllMapped(RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'), (Match m) => "${m[1]},")} TL",
+                      style: kTextStyle.copyWith(color: Colors.white)),
+                )
+              ],
+            ),
           ),
         ),
-      ),
-    ],
+      ],
+    ),
   );
 }
